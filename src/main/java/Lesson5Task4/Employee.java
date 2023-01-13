6d933674e8a1f0dca3314a6013b2d9efd9dd251b
Patrycja <patiwojcik1@gmail.com>
2023-01-13 22:27:40 +0100

    Utworzono klasę Employee

---
+++ src/main/java/Lesson5Task4/Employee.java
@@ -0,0 +1,49 @@
+package Lesson5Task4;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    private final String name;
+    private final String surname;
+    private int age;
+    private BigDecimal baseSalary;
+    private BigDecimal bonus;
+    private final String WHITE_SPACE = "";
+
+    public Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Employee(String name, String surname, int age) {
+        this(name, surname);
+        this.age = age;
+    }
+
+    public Employee(String name, String surname, int age, BigDecimal sumOfSalary) {
+        this(name, surname, age);
+        this.baseSalary = sumOfSalary;
+    }
+
+    public Employee(String name, int age, BigDecimal value1, BigDecimal sumOfBonus, String surname) {
+        this(name, surname, age);
+        this.bonus = sumOfBonus;
+    }
+
+    public String getEmployee_Info() {
+        return "Employee name :" + name + "" + surname;
+    }
+
+    public String getEmployeeDetails() {
+        return "Employee details :" + name + "" + surname + "is" + age;
+    }
+
+    public BigDecimal getSalaryAndBonus() {
+        return baseSalary.add(bonus);
+    }
+
+    public BigDecimal getSalary() {
+        return baseSalary;
+
+    }
+}
