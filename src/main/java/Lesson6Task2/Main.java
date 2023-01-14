428d7a9411955f32d744355c959cb20138c6a71a
Patrycja <patiwojcik1@gmail.com>
2023-01-14 19:42:19 +0100

    Utworzono klasę Main

---
+++ src/main/java/Lesson6Task2/Main.java
@@ -0,0 +1,9 @@
+package Lesson6Task2;
+
+import java.io.File;
+
+public class Main {
+        public static void main(String[] args) {
+            File file = new File("C:\\data.txt");
+
+        }}
