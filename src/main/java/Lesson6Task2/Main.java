b0fc6825383d9335b01228ab2938ffe0bf2d9361
Patrycja <patiwojcik1@gmail.com>
2023-01-14 20:04:58 +0100

    Utworzono klasę Data

--- src/main/java/Lesson6Task2/Main.java
+++
@@ -1,9 +0,0 @@
-package Lesson6Task2;
-
-import java.io.File;
-
-public class Main {
-        public static void main(String[] args) {
-            File file = new File("C:\\data.txt");
-
-        }}
