cfaa9520ef9223863f50f19822624de079c4f3eb
Patrycja <patiwojcik1@gmail.com>
2023-01-14 16:44:46 +0100

    Utworzono klasę Main

--- src/main/java/Lesson6Task1/Main.java
+++ src/main/java/Lesson6Task1/Main.java
@@ -1,11 +1,15 @@
 package Lesson6Task1;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
+    private String pathToFile;
+
     public static void main(String[] args) {
         File file = new File("C:\\data.txt");
 
@@ -14,4 +18,17 @@ public class Main {
             System.out.println(text);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }}}
+        }
+    }
+
+
+    public void writeToFile(String pathToFile, String text) {
+        this.pathToFile = pathToFile;
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(pathToFile))) {
+            writer.write(text);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
