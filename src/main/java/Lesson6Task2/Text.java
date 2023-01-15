d971af7c24bc268b42ef1187a0ab290b247829f4
Patrycja <patiwojcik1@gmail.com>
2023-01-15 13:30:45 +0100

    Utworzono klasę Text

--- src/main/java/Lesson6Task2/Text.java
+++
@@ -1,8 +0,0 @@
-package Lesson6Task2;
-
-import java.io.File;
-
-public class Text {
-    public class Main {
-        public static void main(String[] args) {
-            File file = new File("C:\\data.txt");
