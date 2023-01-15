9024de5039510597a3f98563818ed9514d33106a
Patrycja <patiwojcik1@gmail.com>
2023-01-15 13:55:28 +0100

    Utworzono klasę FileWordSearch

--- src/main/java/Lesson6Task4/FileWordSearch.java
+++ src/main/java/Lesson6Task4/FileWordSearch.java
@@ -2,19 +2,16 @@ package Lesson6Task4;
 
 public class FileWordSearch {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\hp\\Desktop\\TextReader.txt");
-        FileInputStream fileInputStream = new FileInputStream(file);
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String line;
-        int CharacterCount = 0;
-        while ((line = bufferedReader.readLine()) != null) {
-            if (line.equals("")) {
-                paraCount += 1;
-            } else {
-                characterCount += line.length();
-
+        public static void main (String[]args){
+            File file = new File("C:\\data.txt");
+            try {
+                String text = Files.readString(Path.of("C:\\data.txt"));
+                System.out.println(text);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
+            writeToFile("C:\\data.txt");
         }
+
     }
 }
