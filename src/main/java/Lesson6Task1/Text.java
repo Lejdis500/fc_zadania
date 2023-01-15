82c3699e6903b0b6ff00388d6ba6d2c560a2a0ea
Patrycja <patiwojcik1@gmail.com>
2023-01-15 13:44:38 +0100

    Utworzono klasę Text

--- src/main/java/Lesson6Task1/Text.java
+++ src/main/java/Lesson6Task1/Text.java
@@ -6,12 +6,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Text {
-        public static void main(String[] args) {
-            File file = new File("C:\\data.txt");
-            try {
-                String text = Files.readString(Path.of("C:\\data.txt"));
-                System.out.println(text);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }}
+    public static void main(String[] args) {
+        File file = new File("C:\\data.txt");
+        try {
+            String text = Files.readString(Path.of("C:\\data.txt"));
+            System.out.println(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        writeToFile ("C:\\data.txt");
+    }
+
+
+    public void writeToFile(String pathToFile, String text) {
+        try {
+            Files.writeString(Paths.get(pathToFile), text);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
