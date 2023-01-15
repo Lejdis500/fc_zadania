06fe1c81af73b87b565416d6f6ee459c2e2bb2da
Patrycja <patiwojcik1@gmail.com>
2023-01-15 13:50:27 +0100

    Utworzono klasę Character

--- src/main/java/Lesson6Task3/Character.java
+++ src/main/java/Lesson6Task3/Character.java
@@ -1,17 +1,20 @@
 package Lesson6Task3;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Character {
-    public static void main(String[] args) {
-        try {
-            String contect = Files.readString(Paths.get("C:\\data.txt"));
-            System.out.println(contect);
-            System.out.println(contect.length());
-        } catch (I0Exception e) {
-            e.printStackTrace();
+        public static void main(String[] args) {
+            File file = new File("C:\\data.txt");
+            try {
+                String contect = Files.readString(Path.of("C:\\data.txt"));
+                System.out.println(contect);
+                System.out.println(contect.length());
+            } catch (IOException e) {
+            }
+               e.printStackTrace ();
+            }
         }
-    }
-}
