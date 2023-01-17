7239d48280d424b5ce9925b44376d1aff55a14d6
Patrycja <patiwojcik1@gmail.com>
2023-01-17 20:03:27 +0100

    Utworzono klasę Data

--- src/main/java/Lesson6Task0/Data.java
+++ src/main/java/Lesson6Task0/Data.java
@@ -1,13 +1,25 @@
 package Lesson6Task0;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Data {
     public static void main(String[] args) {
-        File file = new File("C:\\data.txt");
-        String fileName = file.getName();
-        boolean isReadable = file.canRead();
-        boolean exist = file.exists();
+        try {
+            Files.writeString(Paths.get("src/main/resources/data.txt"), "Kurs\n" +
+                    "Java\n" +
+                    "Lekcja 6\n" +
+                    "Pliki\n" +
+                    "Wyjątki\n" +
+                    "Pliki\n" +
+                    "Koniec pliku");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    }}
+    }
+}
 
