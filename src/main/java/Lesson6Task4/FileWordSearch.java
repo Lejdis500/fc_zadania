0ebe18843ceafa9e11213f2769758a2d0df96b4c
Patrycja <patiwojcik1@gmail.com>
2023-01-17 21:02:01 +0100

    Utworzono klasę FileWordSearch

--- src/main/java/Lesson6Task4/FileWordSearch.java
+++ src/main/java/Lesson6Task4/FileWordSearch.java
@@ -1,17 +1,21 @@
 package Lesson6Task4;
 
-public class FileWordSearch {
-    public static void main(String[] args) throws IOException {
-        public static void main (String[]args){
-            File file = new File("C:\\data.txt");
-            try {
-                String text = Files.readString(Path.of("C:\\data.txt"));
-                System.out.println(text);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            writeToFile("C:\\data.txt");
-        }
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class FileWordSearch {
+    static void searchWordInFile(String path, String word) {
+        try {
+            String text = Files.readString(Path.of(path));
+            System.out.println(text);
+            if (text.contains(word)) {
+                System.out.println("Plik zawiera szukane słowo");
+            } else {
+                System.out.println("Plik nie zawiera szukanego słowa");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/Lesson6Task4/Main.java
@@ -0,0 +1,7 @@
+package Lesson6Task4;
+
+public class Main {
+    public static void main(String[] args) {
+        FileWordSearch.searchWordInFile("src/main/resources/data.txt", "Pliki");
+    }
+}
