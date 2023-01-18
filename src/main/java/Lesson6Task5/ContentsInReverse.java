febb8298e1dc388a669b345b4d60a541c014daf0
Patrycja <patiwojcik1@gmail.com>
2023-01-18 11:53:01 +0100

    Utworzono klasę ContentsInReverse

---
+++ src/main/java/Lesson6Task5/ContentsInReverse.java
@@ -0,0 +1,22 @@
+package Lesson6Task5;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ContentsInReverse {
+    static void ReserveLineContents (String path) {
+        try {
+            String content = Files.readString(Path.of(path));
+            System.out.println(content);
+            ReserveLineContents ReserveLineContents = new ReserveLineContents (file, "data.txt");
+            String line;
+
+            while ((line = reverseLineReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Lesson6Task5/Main.java
+++ src/main/java/Lesson6Task5/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package Lesson6Task5;
 
 public class Main {
     public static void main(String[] args) {
