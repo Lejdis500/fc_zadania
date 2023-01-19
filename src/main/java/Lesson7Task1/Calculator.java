a54fd56c9939109bdd0ffaa40571360105cac6c7
Patrycja <patiwojcik1@gmail.com>
2023-01-19 21:56:18 +0100

    Utworzono klasę CalculatorTest.java

--- src/main/java/Lesson7Task1/Calculator.java
+++ src/main/java/Lesson7Task1/Calculator.java
@@ -1,16 +1,20 @@
 package Lesson7Task1;
 
 public class Calculator {
-    public static String isDivisibleBy2(int value) {
-        if (value % 2 == 0) {
-            return "true";
-        } else if (value % 2 == 1) {
-            return "false";
-        } else
-            return "error";
+    public static void main(String[] args) {
+        System.out.println("test");
     }
 
-    public static int sum(int value1, int value2, int value3) {
-        return value1 + value2 + value3;
+    public static boolean isOdd(int n) {
+        return n % 2 == 0;
+    }
+
+    public static int sum(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            String num = str.substring(i, i + 1);
+            sum += Integer.parseInt(num);
+        }
+        return sum;
     }
 }
--- src/test/java/Lesson7Task1/CalculatorTest.java
+++ src/test/java/Lesson7Task1/CalculatorTest.java
@@ -1,18 +1,23 @@
 package Lesson7Task1;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 15})
+    public void checkIfOdd(int number) {
+        boolean result = Calculator.isOdd(number);
+        assertEquals(number % 2 == 0, result);
+
+    }
 
     @ParameterizedTest
-    @ValueSource(ints = {2, 6, 18, 66, 100})
-    void shouldVerifyNumberIsEven(int input) {
-        Assertions.assertTrue(Calculator.isEven(input));
-    }
-    @ParameterizedTest
-    @ValueSource(ints = {4,3,2})
-void shouldReturnSum (int input) {
-        Assertions.assertTrue
+    @CsvSource({"123, 6", "456, 15", "789, 24"})
+    public void checkSum(String strNumber, int expected) {
+        int sum = Calculator.sum(strNumber);
+        assertEquals(expected, sum);
     }
 }
+
 
 
