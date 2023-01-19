4aabefdfcffc7e8fe54e758a10a7549cd062e875
Patrycja <patiwojcik1@gmail.com>
2023-01-19 22:07:55 +0100

    Utworzono klasę CalculatorTest.java

--- src/main/java/Lesson7Task2/UpperCase.java
+++ src/main/java/Lesson7Task2/UpperCase.java
@@ -1,9 +1,6 @@
 package Lesson7Task2;
 
 public class UpperCase {
-    public static void main(String[] args) {
-        String txt = "Hello World";
-        System.out.println(txt.toUpperCase());
-    }
-}
-
+    public static String toUpperCase(String str) {
+        return str != null ? str.toUpperCase() : null;
+}}
--- src/test/java/Lesson7Task2/UpperCaseTest.java
+++ src/test/java/Lesson7Task2/UpperCaseTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class UpperCaseTest {
     @ParameterizedTest
     @NullAndEmptySource
-    @ValueSource(strings = {"Hello World"})
-    void shoudVerifyCaseUpper(String input) {
-        Assertions.assertFalse(Case.isUpper(input));
-    }}
+    @ValueSource(strings = {"Hello Words"})
+    public void checkIfUpperCase(String str) {
+        String result = UpperCase.toUpperCase(str);
+        assertEquals(str != null ? str.toUpperCase() : null, result);
+    }
+}
