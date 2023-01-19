972c67093bc92b2d0901a4b404a9108ddef17e75
Patrycja <patiwojcik1@gmail.com>
2023-01-19 19:17:55 +0100

    Utworzono klasę BaseEmployee

---
+++ src/main/java/Lesson3Task3/BaseEmployee.java
@@ -0,0 +1,31 @@
+package Lesson3Task3;
+
+public class BaseEmployee {
+    private static final int baseSalary = 3000;
+    private final String name;
+    private final String surname;
+    private final int employmentYear;
+    private final int salary;
+
+    public BaseEmployee(String name, String surname, int employmentYear, int employmentYear1, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.employmentYear = employmentYear1;
+        this employmentYear = employmentYear;
+        this.salary = salary;
+    }
+        public BaseEmployee(String name, String surname, int employmentYear) {
+        this.name = name;
+        this.surname = surname;
+        this.employmentYear = employmentYear;
+        this.salary = baseSalary;
+    }
+    public int getEmploymentYears () {
+        return 2020-employmentYear;
+    }
+    public int getSalary(){
+        return this.salary;
+    }
+    public substract int calculateMonthlySalary ();
+
+}
---
+++ src/main/java/Lesson3Task3/Manager.java
@@ -0,0 +1,27 @@
+package Lesson3Task3;
+
+public class Manager extends BaseEmployee {
+    private static final int baseSalary = 5000;
+    private static final int baseBonus = 500;
+    private int bonus;
+
+    public Manager(String name, String surname, int employmentYear, int salary) {
+        super(name, surname,employmentYear,salary);
+        this.bonus = baseBonus;
+    }
+
+    @Override
+    int calculateMonthlySalary() {
+        return super.calculateMonthlySalary(){ return this.getSalary() + bonus ;}
+
+
+
+    }
+
+    public Manager(String name, String surname, int employmentYear) {
+        super(name, surname,employmentYear,baseSalary);
+        this.bonus = baseBonus;
+
+
+    }
+}
