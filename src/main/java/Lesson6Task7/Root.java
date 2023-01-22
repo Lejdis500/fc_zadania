31c51c61e146504d57d2057bbafd08652592d858
Patrycja <patiwojcik1@gmail.com>
2023-01-22 16:28:21 +0100

    Utworzono klasę Root

--- src/main/java/Lesson6Task7/Root.java
+++ src/main/java/Lesson6Task7/Root.java
@@ -1,14 +1,10 @@
 package Lesson6Task7;
 
 public class Root {
-    public static void main (String [] args) {
-
-        int a = -4;
-    double sqrt = Math.sqrt(a);
-
-
+    public static double getSquareRoot(double number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+        }
+        return Math.sqrt(number);
     }
-
-
-
 }
