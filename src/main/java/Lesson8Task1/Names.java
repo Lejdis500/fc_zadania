3a675853cbf5f68f00df23a1264834df1b9a40fb
Patrycja <patiwojcik1@gmail.com>
2023-01-22 15:28:19 +0100

    Utworzono klasę Names

--- src/main/java/Lesson8Task1/Names.java
+++ src/main/java/Lesson8Task1/Names.java
@@ -3,9 +3,9 @@ package Lesson8Task1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Names{
-    public static void main (String [] args) {
-        List <String> names = new ArrayList<>();
+public class Names {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
         names.add("Anna");
         names.add("Katarzyna");
         names.add("Tymon");
@@ -17,20 +17,22 @@ public class Names{
         List<String> uniqueNames = new ArrayList<>();
         for (String name : names) {
             if (!uniqueNames.contains(name)) {
-                uniqueNames.add(name); }}
+                uniqueNames.add(name);
+            }
+        }
         System.out.println("List without duplicates" + uniqueNames);
         List<String> reverseNames = new ArrayList<>();
-        for (int i = uniqueNames.size() -1; i>= 0; i-- ) {
+        for (int i = uniqueNames.size() - 1; i >= 0; i--) {
             reverseNames.add(uniqueNames.get(i));
-            }
+        }
         System.out.println("Elements in reverse order:" + reverseNames);
         String changeFrom = "Anna";
         String changeTo = "Joanna";
-        for (int i =0; i < reverseNames.size(); i++) {
+        for (int i = 0; i < reverseNames.size(); i++) {
             if (reverseNames.get(i).equals(changeFrom)) {
-                reverseNames.set(i,changeTo);
+                reverseNames.set(i, changeTo);
             }
         }
-        System.out.println("Elements after change: " + changeFrom +"" + changeTo + "");
-        }
+        System.out.println("Elements after change: " + changeFrom + "->" + changeTo + "");
+    }
 }
