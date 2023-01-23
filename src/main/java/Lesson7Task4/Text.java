eba3b0bbdc5586018715d212dcfc135227ab4d62
Patrycja <patiwojcik1@gmail.com>
2023-01-23 19:01:33 +0100

    Utworzono klasę Text

--- src/main/java/Lesson7Task4/Text.java
+++ src/main/java/Lesson7Task4/Text.java
@@ -1,8 +1,7 @@
 package Lesson7Task4;
 
 public class Text {
-    public static void main(String args[]) {
-        String name = "educative";
-        int length = name.length();
-        System.out.println("The length of the String \"" + name + "\" is: " + length);
-    }}
+    public static int getLength(String text) {
+        return text.length();
+    }
+}
---
+++ src/test/java/Lesson7Task4/TextTest.java
@@ -0,0 +1,27 @@
+package Lesson7Task4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class TextTest {
+
+    @ParameterizedTest
+    @MethodSource("provideLengthStringOfText")
+    void shouldVerifyLengthStringOfText(String input, int expected) {
+        int actualValue = Text.getLength(input);
+        Assertions.assertEquals(expected, actualValue);
+    }
+
+    public static Stream<Arguments> provideLengthStringOfText() {
+        return Stream.of(
+                Arguments.of("", 0),
+                Arguments.of("a", 1),
+                Arguments.of("ala ma kota", 11)
+        );
+    }
+
+}
