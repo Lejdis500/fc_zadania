3f3cdab3588c807d2e0ce63052b7bc4677edef55
Patrycja <patiwojcik1@gmail.com>
2023-01-27 12:13:15 +0100

    Utworzono klasę ContentsInReverse

--- src/main/java/Lesson6Task5/ContentsInReverse.java
+++ src/main/java/Lesson6Task5/ContentsInReverse.java
@@ -3,20 +3,21 @@ package Lesson6Task5;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ContentsInReverse {
-    static void ReserveLineContents(String path) {
+    public static void ReserveLineContents(String path, String pliki) {
         try {
-            String content = Files.readString(Path.of(path));
-            System.out.println(content);
-            ReserveLineContents ReserveLineContents = new ReserveLineContents(file, "data.txt");
-            String line;
-
-            while ((line = reverseLineReader.readLine()) != null) {
-                System.out.println(line);
-            }
+            List<String> allLines = Files.readAllLines(Path.of(path));
+            printReversePart(allLines);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void printReversePart(List<String> allLines) {
+        for (int i = allLines.size() - 1; i > 0; i--) {
+            System.out.println(allLines.get(i));
         }
     }
 }
