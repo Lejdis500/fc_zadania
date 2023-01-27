d8d596c8af167d68111f4c7d2ae25d14ae40306e
Patrycja <patiwojcik1@gmail.com>
2023-01-27 14:03:36 +0100

    Utworzono klasę TextLenght

--- src/test/java/Lesson7Task3/TextLenghtTest.java
+++ src/test/java/Lesson7Task3/TextLenghtTest.java
@@ -1,5 +1,8 @@
 package Lesson7Task3;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 class TextLenghtTest {
     @org.junit.jupiter.api.Test
