e841704d2d622534214a457a77c90808cc831c07
Patrycja <patiwojcik1@gmail.com>
2023-01-28 18:02:21 +0100

    Utworzono klasę PeselValidator

--- src/main/java/Lesson10Task2/Validate.java
+++ src/main/java/Lesson10Task2/Validate.java
@@ -4,7 +4,6 @@ public class Validate {
     public static void validate(String pesel) {
         if (pesel.length() != 11) {
             throw new IllegalArgumentException("Pesel powinien miec 11 znaków");
-
         }
         System.out.println("Hello world!");
     }
