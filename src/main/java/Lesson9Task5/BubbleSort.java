13d6a0b92e637fe3c7c27afb37ce1c5d5f18682e
Patrycja <patiwojcik1@gmail.com>
2023-02-08 12:08:15 +0100

    Utworzono klasę BubbleSort

--- src/main/java/Lesson9Task5/BubbleSort.java
+++ src/main/java/Lesson9Task5/BubbleSort.java
@@ -1,15 +1,12 @@
 package Lesson9Task5;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
 
 public class BubbleSort<T extends Comparable<T>> {
-    public void sort (List <T> list) {
-        int n = list.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (list.get(j).compareTo(list.get(i + 1)) > 0) {
+    public void sort(List<T> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
                     T temp = list.get(j);
                     list.set(j, list.get(j + 1));
                     list.set(j + 1, temp);
